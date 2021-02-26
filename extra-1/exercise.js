08fd00505dfc30eb9c301c11aa93b9d8ca06779e
Mohand Abboud <mohandabboud9@gmail.com>
2021-02-26 23:49:36 +0100

    solving extra-1

--- extra-1/exercise.js
+++ extra-1/exercise.js
@@ -1,5 +1,5 @@
 
-function date(birthd , a , DateNow, yearsLeft , yearsLeftt, retireDate) {
+function date(birthd , a ) {
 var x = prompt("Wirte the number of the day you were born on");
 var y = prompt("Wirte the number of the month you were born in");
 var z = prompt("Wirte the year you were born at");
@@ -7,11 +7,14 @@ var birthd = new Date(y +"-"+x +"-"+z);
 var a = prompt("Wirte the age you wish to retire at");
 var age = Date.now() - birthd;
 var DateNow = age / (1000*60*60*24*365); 
-window.alert(Math.floor(DateNow));
-var yearsLeft = a - DateNow;
-var yearsLeftt = yearsLeft * (1000*60*60*24*365); 
-var retireDate = Date.now() + yearsLeftt;
-window.alert("you have "+ yearsLeft +" until retire it is 2021 now so you will retire at "+ retireDate.toString() +".")
+var da = Math.round(DateNow);
+var yearsLeft = a - da;
+var d = new Date();
+var year = d.getFullYear();
+var month = d.getMonth();
+var yearl = Math.round(yearsLeft);
+var c = new Date(year + yearl, month);
+window.alert("you have "+ yearsLeft +" years until retire, it is "+ year +" now so you will retire at "+ c.toDateString() +".")
 }
 
 date();
