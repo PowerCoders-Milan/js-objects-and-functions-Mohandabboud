58841f55b663a3951c1175d50b5bde683894bd85
Mohand Abboud <mohandabboud9@gmail.com>
2021-02-26 16:49:59 +0100

    solving ex-6

--- ex-6/exercise.js
+++ ex-6/exercise.js
@@ -1,3 +1,22 @@
 // write your JS here
 // remember to always test in the browser
 // to see if everything works correctly
+
+function greaterNum(firstNumber , secondNumber) {
+   
+    if (firstNumber > secondNumber) {
+        return ( "The greater number of " + firstNumber +" and " + secondNumber +" is " + firstNumber + ".");
+    }
+    
+    else if (firstNumber < secondNumber) {
+        return ( "The greater number of " + firstNumber  +" and " + secondNumber +" is " + secondNumber + ".");
+    }
+    
+    else {
+        return ("The numbers are the same.");
+    }
+     
+}
+
+console.log(greaterNum(10,5));
+console.log(greaterNum(15,20));
