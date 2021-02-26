c9ef680d01ae4ae7c17ad02c76d866b2bc7b2e64
Mohand Abboud <mohandabboud9@gmail.com>
2021-02-26 12:40:40 +0100

    solving ex-2

--- ex-2/exercise.js
+++ ex-2/exercise.js
@@ -1,3 +1,19 @@
 // write your JS here
 // remember to always test in the browser
 // to see if everything works correctly
+
+function tellFortune( job ,  geo , partner , children) {
+    this.job = job 
+    this.geo= geo
+    this.partner= partner
+    this.children= children
+    var job = prompt("write your dream job");
+    var geo = prompt("write where you want it to be");
+    var partner = prompt("write your future partner name ");
+    var children = prompt("write how many children you wish to have"); 
+    window.alert("You will be a " + job +" in " + geo +", and married to " + partner + " with " + children +" kids."); 
+}
+
+tellFortune();
+tellFortune();
+tellFortune();
