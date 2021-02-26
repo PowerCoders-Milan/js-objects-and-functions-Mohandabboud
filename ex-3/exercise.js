eecd3208c10abe511a046d864425b754d71bc987
Mohand Abboud <mohandabboud9@gmail.com>
2021-02-26 14:38:52 +0100

    solving ex-3

--- ex-3/exercise.js
+++ ex-3/exercise.js
@@ -1,3 +1,32 @@
 // write your JS here
 // remember to always test in the browser
 // to see if everything works correctly
+
+var choices = ["blue", "apple" , "reading", "walking"];
+console.log("my favorite colour is " + choices[0]);
+console.log("my favorite fruit is " + choices[1]);
+console.log("my favorite hobby is " + choices[2]);
+console.log("my favorite sport is " + choices[3]);
+console.log( choices);
+
+
+
+var choicces = [ drinks = {name:"tea",type:"black"}, sports = {name:"running",type:"short distance"}];
+console.log(drinks.name);
+
+window.alert(drinks.type+" "+ drinks.name);
+window.alert(sports.name +" "+ sports.type);
+
+var drinkss = {
+    name:"tea",
+    type:"black",
+}
+var sportss = {
+    name:"running",
+    type:"short distances",
+}
+
+var choicees = [ drinkss , sportss];
+console.log(choicees);
+window.alert(drinkss.type +" "+ drinkss.name);
+window.alert(sportss.name +" "+ sportss.type);
